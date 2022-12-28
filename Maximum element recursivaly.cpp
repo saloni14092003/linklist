3c05c60e36302df64c86e88c585876d60d662b1c
saloni14092003 <80242006+saloni14092003@users.noreply.github.com>
2022-12-28 23:38:55 +0530

    Update Maximum element recursivaly.cpp

--- Maximum element recursivaly.cpp
+++ Maximum element recursivaly.cpp
@@ -1,6 +1,42 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+// find the maximum element in the linklist with the help of recurssion
+
+#include <bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    struct Node*next;
+}*first=NULL;
+void create(int a[],int n){
+    struct Node *temp,*last;
+    first= new Node;
+    first->data=a[0];
+    first->next=NULL;
+    last=first;
+    for(int i=1;i<n;i++){
+        temp=new Node;
+        temp->data=a[i];
+        temp->next=NULL;
+        last->next=temp;
+        last=temp;
+    }
+}
+
+int max_recursion(struct Node* p){
+    int x=0;
+    if(p==0){
+        return 0;
+    }
+    else{
+        x=max_recursion(p->next);
+        if(p->data>x)
+          return p->data;
+          return x;
+    }
+    
+}
+int main(){
+    int a[]={9,8,5,2,12};
+    create(a,5);
+    cout<<max_recursion(first);
+    return 0;
 }
