d9b36b59b9bf66810713e188dcef3fd8debc6713
saloni14092003 <80242006+saloni14092003@users.noreply.github.com>
2022-12-28 23:30:45 +0530

    Update recursivaly_display_reverse.cpp

--- recursivaly_display_reverse.cpp
+++ recursivaly_display_reverse.cpp
@@ -1,6 +1,35 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <iostream>
+using namespace std;
+struct Node{
+    int data;
+    struct Node*next;
+} *first=NULL;
+
+void create(int a[],int n){
+    struct Node*temp,*last;
+    first=new Node;
+    first->data=a[0];
+    first->next=NULL;
+    last=first;
+for(int i=1;i<n;i++){
+    temp=new Node;
+    temp->data=a[i];
+    temp->next=NULL;
+    last->next=temp;
+    last=temp;
+}
+}
+void Recurrsive_Display_reverse(struct Node*p){
+    if(p!=NULL){
+        Recurrsive_Display_reverse(p->next);
+        cout<<p->data<<" ";
+    }
+}
+
+int main()
+{
+   int array[]={1,6,7,8,9,3};
+   create(array,6);
+   Recurrsive_Display_reverse(first);
+    return 0;
 }
