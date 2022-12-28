59e29c57a1ad5ce80250c6f3fa02ec26a376b1b3
saloni14092003 <80242006+saloni14092003@users.noreply.github.com>
2022-12-28 23:41:28 +0530

    Update Minimum element.cpp

--- Minimum element.cpp
+++ Minimum element.cpp
@@ -1,6 +1,39 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+// find the minimum element in the linklist 
+
+#include <bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    struct Node*next;
+}*first=NULL;
+void create(int a[],int n){
+    struct Node *temp,*last;
+    first= new Node;
+    first->data=a[0];
+    first->next=NULL;
+    last=first;
+    for(int i=1;i<n;i++){
+        temp=new Node;
+        temp->data=a[i];
+        temp->next=NULL;
+        last->next=temp;
+        last=temp;
+    }
+}
+
+int min(struct Node*p){
+   int m=3278;
+   while(p!=0){
+       if(p->data<m)
+         m=p->data;
+         p=p->next;
+   }
+   return(m);
+}
+
+int main(){
+    int a[]={9,8,5,2,12};
+    create(a,5);
+    cout<<min(first);
+    return 0;
 }
