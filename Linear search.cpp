3a80bb6d6b17357bdfc3a6b674c70a62b8ccfbbf
saloni14092003 <80242006+saloni14092003@users.noreply.github.com>
2022-12-28 23:43:37 +0530

    Update Linear search.cpp

--- Linear search.cpp
+++ Linear search.cpp
@@ -1,6 +1,44 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+//linear search without recursion
+#include<iostream>
+#include <climits>
+using namespace std;
+struct Node{
+    int data;
+    struct Node*next;
+}*first=NULL;
+void create(int a[],int n){
+    struct Node *temp,*last;
+    first= new Node;
+    first->data=a[0];
+    first->next=NULL;
+    last=first;
+    for(int i=0;i<n;i++){
+        temp=new Node;
+        temp->data=a[i];
+        temp->next=NULL;
+        last->next=temp;
+        last=temp;
+    }
+}
+struct Node *linear_search(struct Node*p,int key){
+    while(p!=NULL){
+        if(key==p->data)
+          return p;
+          p=p->next;
+            
+        }
+    return NULL;
+}
+int main(){
+    struct Node*temp;
+    int a[]={8,9,3,4,2};
+    create(a,5);
+    temp =linear_search(first,4);
+    if(temp){
+        cout<<"yes"<<endl;
+    }
+    else{
+        cout<<"no"<<endl;
+    }
+    return 0;
 }
