782173a361639865a6a27e7f5be10ca97e270226
saloni14092003 <80242006+saloni14092003@users.noreply.github.com>
2022-12-28 23:36:00 +0530

    Update sum of nodes Recursivaly.cpp

--- sum of nodes Recursivaly.cpp
+++ sum of nodes Recursivaly.cpp
@@ -1,6 +1,33 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+// find the sum of nodes in a linklist recursivaly
+#include <iostream>
+using namespace std;
+struct Node{
+    int data;
+    struct Node*next;
+}*first=NULL;
+void create(int a[],int n){
+    struct Node*temp,*last;
+    first = new Node;
+    first->data=a[0];
+    first->next=NULL;
+    last=first;
+    for(int i=1;i<n;i++){
+    temp=new Node;
+    temp->data=a[i];
+    temp->next=NULL;
+    last->next=temp;
+    last=temp;
+    }
+}
+int sum_recursivaly(struct Node*p){
+    if(p==NULL)
+        return 0;
+        return sum_recursivaly(p->next)+p->data;
+}
+int main()
+{
+   int a[]={4,2,1,6,2,3};
+   create(a,6);
+   cout<<sum_recursivaly(first);
+    return 0;
 }
